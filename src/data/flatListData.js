6a978112eecc96b782ec6a90db97bdb8f9648da6
nguyenhoanglong0106 <49869534+nguyenhoanglong0106@users.noreply.github.com>
2020-12-23 22:57:47 +0700

    check

---
+++ src/data/flatListData.js
@@ -0,0 +1,39 @@
+var flatListData = [
+    {
+        key:'0',
+        tenphong: "Phòng Nhân Sự",
+        imageUrl:"https://www.verywellhealth.com/thmb/0mTCeKqywiovRWMiugc4mN99CGQ=/2880x1918/filters:fill(87E3EF,1)/fruits-to-avoid-if-you-have-diabetes-1087587-primary-recirc-3a95a09a48cb46b49d5632326f9405d3.jpg",
+        soluongnguoi:15,
+    },
+    {
+        key:'1',
+        tenphong: "Phòng Kế Toán",
+        imageUrl:"https://firebasestorage.googleapis.com/v0/b/quanlynhansurn.appspot.com/o/ImageFood%2FTuanPhong?alt=media&token=2c4f8f02-26e7-4990-8161-d8e56d0eb820",
+        soluongnguoi:16,
+    },
+    {
+        key:'2',
+        tenphong: "Phòng Kế Hoạch",
+        imageUrl:"https://firebasestorage.googleapis.com/v0/b/quanlynhansurn.appspot.com/o/ImageFood%2FThanhNhan?alt=media&token=dd5129f0-e61d-4880-a0a6-d33da68b49ae",
+        soluongnguoi:17,
+    },
+    {
+        key:'3',
+        tenphong: "Phòng Ý Tưởng",
+        imageUrl:"https://firebasestorage.googleapis.com/v0/b/quanlynhansurn.appspot.com/o/ImageFood%2FHoangLong?alt=media&token=4cd01f68-47a3-47ba-b6de-d237ee0d8678",
+        soluongnguoi:18,
+    },
+    {
+        key:'4',
+        tenphong: "Phòng Tài Vụ",
+        imageUrl:"https://www.verywellhealth.com/thmb/0mTCeKqywiovRWMiugc4mN99CGQ=/2880x1918/filters:fill(87E3EF,1)/fruits-to-avoid-if-you-have-diabetes-1087587-primary-recirc-3a95a09a48cb46b49d5632326f9405d3.jpg",
+        soluongnguoi:19,
+    },
+    {
+        key:'5',
+        tenphong: "Phòng Giám Hiệu",
+        imageUrl:"https://www.verywellhealth.com/thmb/0mTCeKqywiovRWMiugc4mN99CGQ=/2880x1918/filters:fill(87E3EF,1)/fruits-to-avoid-if-you-have-diabetes-1087587-primary-recirc-3a95a09a48cb46b49d5632326f9405d3.jpg",
+        soluongnguoi:20,
+    }
+]; 
+module.exports = flatListData;
